fa7b0e130f682bb1c9a9a9eeabfeb62dcc622fab
Ronan Boiteau <boiteauronan@gmail.com>
2018-01-14 19:38:27 +0100

    Removed header in string.h

--- include/string.h
+++ include/string.h
@@ -1,13 +1,3 @@
-/*
-** string.h for libmy in /home/boitea_r
-** 
-** Made by Ronan Boiteau
-** Login   <boitea_r@epitech.net>
-** 
-** Started on  Tue Nov 10 16:11:56 2015 Ronan Boiteau
-** Last update Tue Nov 10 16:11:57 2015 Ronan Boiteau
-*/
-
 #ifndef STRING_H_
 # define STRING_H_
 
